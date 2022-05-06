9f81c8a8be7c489962166d6b52e86595514e18a0
SUNGCHIHUN <clgnsdl94@gmail.com>
2022-05-06 15:43:02 +0900

    222-05-06 15:42 JPA 등록, 수정, 삭제

--- src/main/java/com/springboot/ex02/product/controller/ProductController.java
+++ src/main/java/com/springboot/ex02/product/controller/ProductController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.springboot.ex02.product.service.ProductService;
 import com.springboot.ex02.product.vo.Product;
@@ -16,6 +20,7 @@ public class ProductController {
 	@Autowired
 	ProductService service;
 	
+	// 리스트
 	@RequestMapping("/")
 	public String viewHome(Model model) {
 		
@@ -24,5 +29,43 @@ public class ProductController {
 		
 		return "index"; // 타임리프 방식임 => template/index.html
 	};
+
+	// 제품 등록화면
+	@RequestMapping("/new")
+	public String newProductForm(Model model) {
+		
+		Product product = new Product();
+		model.addAttribute("product", product);
+		
+		return "new_product";
+	}
 	
+	// 제품 등록, 수정
+	@PostMapping("/save")
+	public String saveProduct(@ModelAttribute("product") Product product, Model model) {
+		
+		service.save(product);
+		
+		return "redirect:/"; // 한건이 추가된 후 index.html로 이동
+	};
+	
+	// 제품 수정 화면
+	@RequestMapping("/edit/{id}")
+	public ModelAndView editProductForm(@PathVariable(name="id") int id) {
+		
+		ModelAndView mav = new ModelAndView();
+		Product product = service.get(id); // 해당 id로 한건 조회
+		mav.setViewName("edit_product");
+		mav.addObject(product);
+		
+		return mav;
+	}
+	
+	// 제품 삭제
+	@RequestMapping("/delete/{id}")
+	public String deleteProduct(@PathVariable(name="id") int id) {
+		
+		service.delete(id);
+		return "redirect:/";
+	}
 }
